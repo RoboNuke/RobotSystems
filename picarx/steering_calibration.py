27774190d80ba0894838aa403699bdb34d99ffa7
RoboNuke <hunter.brown4762@gmail.com>
2024-01-25 13:20:01 -0800

    Fixed steering calibration script for trial and error

--- picarx/steering_calibration.py
+++ picarx/steering_calibration.py
@@ -4,6 +4,8 @@ import math
 
 if __name__=="__main__":
     unhappy = True
+    px = Picarx()
+    px.dir_servo_calibrate(0.0)
     while unhappy:
 
         #dist = float(input("Distance of horizontal offset:"))
@@ -15,14 +17,15 @@ if __name__=="__main__":
         offset_angle=float(input("Input offset angle: "))
         print(f"Steering is off by an angle of {offset_angle}")
 
-        px = Picarx()
         px.set_dir_servo_angle(offset_angle)
-        px.forward(25)
+        px.forward(50)
         time.sleep(2)
         px.stop()
 
-        unhappy = bool(print("Are you happy? "))
+        unhappy = not 'y' == input("Are you happy? ")
 
-        px.dir_servo_calibrate(offset_angle)
+    
+    px.dir_servo_calibrate(offset_angle)
 
     print("Calibration Set, have a good day!")
+
